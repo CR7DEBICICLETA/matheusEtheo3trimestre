2f37a46d9d32c1d220629cfd8cb696c69d33edc5
CR7DEBICICLETA <160138883+CR7DEBICICLETA@users.noreply.github.com>
2024-10-28 08:54:28 -0300

    28/10

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function criaCartao(categoria,pergunta,resposta){
-    console.log(categoria,pergunta,resposta)
+   let container =  document.getElementById('container')
+   let article  =  document.createElement('article')
+   article.className  =  'cartao'
+   article.innerHTML = 'Isso é um teste'
+   container.appendChild(article)
 }
 
+
 criaCartao('biologia', 'qual é a funçao da mitocondria', 'produção de energia')
+
