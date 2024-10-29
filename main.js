22f610e2f65ccec9b959badc52a8371b54a3a7d7
CR7DEBICICLETA <160138883+CR7DEBICICLETA@users.noreply.github.com>
2024-10-29 08:59:16 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -22,3 +22,4 @@ function criaCartao(categoria,pergunta,resposta){
 criaCartao('biologia', 'qual é a funçao da mitocondria', 'produção de energia')
 criaCartao('matematica','como é definido um grupo','é um conjunto de uma operação onde está  é  fechada no conjunto e tem algumas propriedades ')
 criaCartao('Piada','qual é o carro que avisa que irá chover','celta preto')
+criaCartao('pergunta','como comprar um pc gamer?','<img src="img/download.jpeg" alt="">')
