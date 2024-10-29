4ce0c353f61c7ffe9f917a3da9339053924b7f8b
CR7DEBICICLETA <160138883+CR7DEBICICLETA@users.noreply.github.com>
2024-10-29 08:53:53 -0300

    29/10

--- main.js
+++ main.js
@@ -2,10 +2,23 @@ function criaCartao(categoria,pergunta,resposta){
    let container =  document.getElementById('container')
    let article  =  document.createElement('article')
    article.className  =  'cartao'
-   article.innerHTML = 'Isso é um teste'
-   container.appendChild(article)
+   article.innerHTML = `
+   <div class="cartao-conteudo">
+   <h3> ${categoria} </h3>
+   <div class= "cartao-conteudo-pergunta">
+     <p> ${pergunta} </p>
+     </div>
+   <div class= "cartao-conteudo-resposta">
+   <p> ${resposta} </p>
+   </div>
+   </div>
+   
+   `
+
+   container.appendChild(article) 
 }
 
 
 criaCartao('biologia', 'qual é a funçao da mitocondria', 'produção de energia')
-
+criaCartao('matematica','como é definido um grupo','é um conjunto de uma operação onde está  é  fechada no conjunto e tem algumas propriedades ')
+criaCartao('Piada','qual é o carro que avisa que irá chover','celta preto')
